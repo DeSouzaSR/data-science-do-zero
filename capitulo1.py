bd5e4ca9cbf407f9ee21763cd12723c8ced67d8c
DeSouzaSR <sandro.fisica@gmail.com>
2024-06-10 20:49:31 -0300

    Conclui análise de interesses em comum

--- capitulo1.py
+++ capitulo1.py
@@ -90,4 +90,62 @@ def friends_of_friend_ids(user):
 
 print("Amigos do usuário")
 print(users[3]["name"])
-print(friends_of_friend_ids(users[3]))
+print(friends_of_friend_ids(users[3])) 
+print('Chi tem dois amigos em comum com Hero(0) e um com Clive(5)')
+
+###### Usuários com o mesmo interesse
+# Dados
+interests = [
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
+    (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "theory"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
+]
+
+# Função que encontra usuários com o mesmo interesse
+def data_scientists_who_like(target_interest):
+    """Fornece a lista de usuários com o interesse alvo"""
+    return [user_id
+            for user_id, user_interest in interests
+            if user_interest == target_interest]
+
+print("\nCientistas de dados que gostam de Hadoop")
+print(data_scientists_who_like("Hadoop"))
+print("Cientistas de dados que gostam de Python")
+print(data_scientists_who_like("Python"))
+
+# Construir um índice de interesses para usuários 
+# Vamos construir um dicionário em que as chaves são interesses e os valores são
+#   listas de usuários.
+from collections import defaultdict
+user_ids_by_interest = defaultdict(list)
+
+for user_id, interest in interests:
+    user_ids_by_interest[interest].append(user_id)
+
+# Vamos construir um dicionário em que as chaves são os usuários e os valores são
+#   os interesses
+
+interests_by_user_id = defaultdict(list)
+for user_id, interest in interests:
+    interests_by_user_id[user_id].append(interest)
+
+# Agora vamos descobrir quem possui os interesses em comum
+def most_common_interests_with(user):
+    return Counter(interested_user_id
+        for interest in interests_by_user_id[user['id']]
+        for interested_user_id in user_ids_by_interest[interest]
+        if interested_user_id != user['id'])
+
+print("Interesses em comum com Hero(0)")
+print(most_common_interests_with(users[0]))
